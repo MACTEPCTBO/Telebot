835637bb5ecc41ead9ad4e9ed95a9e3800b9f73a
MACTEPCTBO <92295280+MACTEPCTBO@users.noreply.github.com>
2024-06-04 16:50:10 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5128764107:AAGG1UspFPC5qKnHRBfHV0k-57zCfSA6D8o'
+
+text_otvet = {'hello' : 'xуй'}
+text_error = ['Я не понимаю что вы написали','Я молодой бот я не понимаю что вы написали']
+
+
+command_text = ['st','sr']
+command_event = {'/st' : lambda : print('hello'),
+                 '/sr' : lambda : print('h')}
